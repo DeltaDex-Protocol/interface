55aad3dc94f4f7f2e551ace53c42dd388cf66396
partylikeits1983 <aleqvids@gmail.com>
2022-07-21 16:46:15 +0300

    added ethers

--- src/Components/Panel/UserInformation/UserInformation.jsx
+++ src/Components/Panel/UserInformation/UserInformation.jsx
@@ -1,245 +1,298 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // import other component
-import FormInput from '../../Forms/FormInput/FormInput';
-import Titles from '../../Titles/Titles';
+import FormInput from "../../Forms/FormInput/FormInput";
+import Titles from "../../Titles/Titles";
 
 // import other pkg
-import { Form, Row, Col, Button } from 'react-bootstrap';
-import { useFormik } from 'formik';
-import { object, string, date } from 'yup'
-import PropTypes from 'prop-types';
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { useFormik } from "formik";
+import { object, string, date } from "yup";
+import PropTypes from "prop-types";
 
 // import utils
-import { getStorage } from '../../../utils/storage';
+import { getStorage } from "../../../utils/storage";
+import { mintNFT } from "../../../utils/interact";
 
+const UserInformation = ({
+  username,
+  firstName,
+  lastName,
+  email,
+  birthday,
+  onChangeInfo,
+}) => {
+  const [submit, setSubmit] = useState(false);
 
-const UserInformation = ({ username , firstName, lastName, email, birthday, onChangeInfo }) => {
-    const [submit, setSubmit] = useState(false)
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
 
+  const [addressToken0, setaddressToken0] = useState("");
+  const [addressToken1, setaddressToken1] = useState("");
+  const [token0Balance, setToken0Balance] = useState("");
+  const [fees, setFees] = useState("");
+  const [perDay, setPerDay] = useState("");
 
-    return (
-        <>
-            <Titles title='Replicate Your option' text="Choose the parameters of the option you'd like to replicate" />
-            
-            <Form >
-                <Row className="mt-5 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="token0"
-                        controlId=""
-                        text="Token 0"
-                        placeholder="Address of token 0"
-                        size='sm'
-                        successMsg="done"
-                    />
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0 ms-lg-5 mt-3 mt-lg-0"
-                        name="token1"
-                        controlId=""
-                        text="Token 1"
-                        placeholder="Address of token 1"
-                        size='sm'
+  const [strike, setStrike] = useState("");
+  const [expiration, setExpiration] = useState("");
+  const [riskFree, setRiskFree] = useState("");
+  const [volatility, setVolatility] = useState("");
+  const [meanReversion, setMeanReversion] = useState("");
+  const [jumpDeviation, setJumpDeviation] = useState("");
+  const [jumpIntensity, setJumpIntensity] = useState("");
 
-                        successMsg="done"
-                    />
-                </Row>
+  const sendForm = async () => {
+    const { success, status } = await mintNFT(
+      addressToken0,
+      addressToken1,
+      token0Balance,
+      fees,
+      perDay,
+      strike,
+      expiration,
+      riskFree,
+      volatility,
+      meanReversion,
+      jumpDeviation,
+      jumpIntensity
+    );
+  };
 
-                <Row className="mt-3 mt-lg-4 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="amountOfToken0"
-                        controlId=""
-                        text="Amount of token 0"
-                        placeholder="uint256"
-                        size='sm'
+  return (
+    <>
+      <Titles
+        title="Replicate Your option"
+        text="Choose the parameters of the option you'd like to replicate"
+      />
 
-                    />
+      <Form>
+        <Row className="mt-5 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="token0"
+            type="text"
+            controlId=""
+            text="Token 0"
+            placeholder="Address of token 0"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => {
+              setaddressToken0(event.target.value);
+              console.log(addressToken0);
+            }}
+          />
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0 ms-lg-5 mt-3 mt-lg-0"
+            name="token1"
+            type="text"
+            controlId=""
+            text="Token 1"
+            placeholder="Address of token 1"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setaddressToken1(event.target.value)}
+          />
+        </Row>
 
-                </Row>
-                <Row className="mt-3 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0  mt-3 mt-lg-0"
-                        name="fees"
-                        controlId=""
-                        text="Fees you agree to split between your position hedgers"
-                        size='sm'
-                        placeholder="uint256"
+        <Row className="mt-3 mt-lg-4 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="amountOfToken0"
+            type="text"
+            controlId=""
+            text="Amount of token 0"
+            placeholder="uint256"
+            size="sm"
+            onChange={(event) => setToken0Balance(event.target.value)}
+          />
+        </Row>
+        <Row className="mt-3 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0  mt-3 mt-lg-0"
+            name="fees"
+            type="text"
+            controlId=""
+            text="Fees you agree to split between your position hedgers"
+            size="sm"
+            placeholder="uint256"
+            onChange={(event) => setFees(event.target.value)}
+          />
+        </Row>
+        <Row className="mt-3 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="hedgePerDay"
+            type="text"
+            controlId=""
+            text="Number of delta hedges per day"
+            placeholder="uint"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setFees(event.target.value)}
+          />
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0 ms-lg-5 mt-4 mt-lg-3"
+            name="strike"
+            type="text"
+            controlId=""
+            text="Strike Price"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setPerDay(event.target.value)}
+          />
+        </Row>
+        <Row className="mt-3 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="expirationDate"
+            type="text"
+            controlId=""
+            text="Expiration Date"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setExpiration(event.target.value)}
+          />
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0 ms-lg-5 mt-4 mt-lg-0"
+            name="riskFreeRate"
+            type="text"
+            controlId=""
+            text="Risk Free Rate"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setRiskFree(event.target.value)}
+          />
+        </Row>
+        <p className="mt-3 px-1 fw-bold">
+          Choose the parameters of Jump Diffusion Model:
+        </p>
+        <Row className="mt-3 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="sigma"
+            type="text"
+            controlId=""
+            text="Volatility (sigma)"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setVolatility(event.target.value)}
+          />
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="intensityOfJump"
+            type="text"
+            controlId=""
+            text="Intensity Of Jump (lam)"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setJumpIntensity(event.target.value)}
+          />
+        </Row>
+        <Row className="mt-3 px-3">
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0"
+            name="meanRateOfJump"
+            type="text"
+            controlId=""
+            text="Mean of Merton's Jump size (m)"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setMeanReversion(event.target.value)}
+          />
+          <FormInput
+            xs={12}
+            lg
+            as={Col}
+            inpClass="py-2"
+            className="p-0 ms-lg-5 mt-4 mt-lg-0"
+            name="stdOfJump"
+            type="text"
+            controlId=""
+            text="Standart Deviation of Jump size (v)"
+            placeholder="uint256"
+            size="sm"
+            successMsg="done"
+            onChange={(event) => setJumpDeviation(event.target.value)}
+          />
+        </Row>
 
-                    />
-                </Row>
-                <Row className="mt-3 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="hedgePerDay"
-                        controlId=""
-                        text="Number of delta hedges per day"
-                        placeholder="uint"
-                        size='sm'
-                        successMsg="done"
-                    />
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0 ms-lg-5 mt-4 mt-lg-3"
-                        name="strike"
-                        controlId=""
-                        text="Strike Price"
-                        placeholder="uint256"
-                        size='sm'
-
-                        successMsg="done"
-                    />
-                </Row>
-                <Row className="mt-3 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="expirationDate"
-                        controlId=""
-                        text="Expiration Date"
-                        placeholder="uint256"
-                        size='sm'
-                        successMsg="done"
-                    />
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0 ms-lg-5 mt-4 mt-lg-0"
-                        name="riskFreeRate"
-                        controlId=""
-                        text="Risk Free Rate"
-                        placeholder="uint256"
-                        size='sm'
-
-                        successMsg="done"
-                    />
-                </Row>
-                <p className='mt-3 px-1 fw-bold'>Choose the parameters of Jump Diffusion Model:</p>
-                <Row className="mt-3 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="sigma"
-                        controlId=""
-                        text="Volatility (sigma)"
-                        placeholder="uint256"
-                        size='sm'
-                        successMsg="done"
-                    />
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0 ms-lg-5 mt-4 mt-lg-0"
-                        name="riskFreeRate (r)"
-                        controlId=""
-                        text="Risk Free Rate"
-                        placeholder="uint256"
-                        size='sm'
-
-                        successMsg="done"
-                    />
-                </Row>
-                <Row className="mt-3 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="meanRateOfJump"
-                        controlId=""
-                        text="Mean of Merton's Jump size (m)"
-                        placeholder="uint256"
-                        size='sm'
-                        successMsg="done"
-                    />
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0 ms-lg-5 mt-4 mt-lg-0"
-                        name="stdOfJump"
-                        controlId=""
-                        text="Standart Deviation of Jump size (v)"
-                        placeholder="uint256"
-                        size='sm'
-
-                        successMsg="done"
-                    />
-                </Row>
-                <Row className="mt-3 px-3">
-                    <FormInput 
-                        xs={12}
-                        lg
-                        as={Col}
-                        inpClass='py-2'
-                        className="p-0"
-                        name="intensityOfJump"
-                        controlId=""
-                        text="Intensity Of Jump (lam)"
-                        placeholder="uint256"
-                        size='sm'
-                        successMsg="done"
-                    />
-
-                </Row>
-                {/*<Button 
+        {/*<Button 
                     onClick={() => setSubmit(true)}
                     disabled={submit && !formik.isValid ? true : false}
                     variant="primary" className='mt-5 py-2 px-4'
                     type="submit">
                     Update
                 </Button>*/}
-                <Button variant="primary" className="mt-5 py-2 px-40">
-                Start replication
-                </Button>
-            </Form>
-        </>
-    )
-}
+        <Button
+          variant="primary"
+          className="mt-5 py-2 px-40"
+          onClick={sendForm}
+        >
+          Start replication
+        </Button>
+      </Form>
+    </>
+  );
+};
 
 // validate the component
 UserInformation.propTypes = {
-    username: PropTypes.string.isRequired, 
-    firstName: PropTypes.string.isRequired,
-    lastName: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired, 
-    birthday: PropTypes.string.isRequired, 
-    onChangeInfo: PropTypes.func.isRequired,
-}
+  username: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  birthday: PropTypes.string.isRequired,
+  onChangeInfo: PropTypes.func.isRequired,
+};
 
-export default UserInformation
+export default UserInformation;
--- src/utils/interact.js
+++ src/utils/interact.js
@@ -8,43 +8,122 @@
 // const contractABI = require("../contract-abi.json")
 // const contractAddress = "0xd1b04035bB8E12584070f3f42090877Cee52817a";
 
+const contractABI = require("./OptionMaker.json");
+const contractAddress = "0x9Fcca440F19c62CDF7f973eB6DDF218B15d4C71D";
 
+var ethers = require("ethers");
+const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 export const connectWallet = async () => {
-	if (window.ethereum) {
-		try {
-			const addressArray = await window.ethereum.request({
-				method: 'eth_requestAccounts',
-			});
-			const obj = {
-				status: "👆🏽 Write a message in the text-field above.",
-				address: addressArray[0],
-			};
-			return obj;
-		} catch (err) {
-			return {
-				address: "",
-				status: "😥 " + err.message,
-			};
-		}
-	} else {
-		return {
-			address: "",
-			status: (
-				<span>
-					<p>
-						{" "}
-							🦊{" "}
-							<a target="_blank" href={`https://metamask.io/download.html`}>
-							You must install Metamask, a virtual Ethereum wallet, in your
-							browser.
-							</a>
-						</p>
-					</span>
-				),
-		};
-	}
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "👆🏽 Write a message in the text-field above.",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
 
+export const mintNFT = async (
+  addressToken0,
+  addressToken1,
+  token0Balance,
+  fees,
+  perDay,
+  strike,
+  expiration,
+  riskFree,
+  volatility,
+  meanReversion,
+  jumpDeviation,
+  jumpIntensity
+) => {
+  if (addressToken0.trim() == "") {
+    return {
+      success: false,
+      status: "❗Please make sure all fields are completed before minting.",
+    };
+  }
+
+  token0Balance = ethers.utils.parseUnits(token0Balance);
+  fees = ethers.utils.parseUnits(fees);
+  perDay = ethers.utils.parseUnits(perDay);
+  strike = ethers.utils.parseUnits(strike);
+  expiration = ethers.utils.parseUnits(expiration);
+  riskFree = ethers.utils.parseUnits(riskFree);
+  volatility = ethers.utils.parseUnits(volatility);
+  meanReversion = ethers.utils.parseUnits(meanReversion);
+  jumpDeviation = ethers.utils.parseUnits(jumpDeviation);
+  jumpIntensity = ethers.utils.parseUnits(jumpIntensity);
+
+  // JDM input
+  const JDM_Call_Input = [
+    addressToken0,
+    addressToken1,
+    token0Balance,
+    0,
+    0,
+    0,
+    fees,
+    perDay,
+    0,
+    0,
+    [
+      strike,
+      expiration,
+      riskFree,
+      volatility,
+      meanReversion,
+      jumpDeviation,
+      jumpIntensity,
+    ],
+  ];
+
+  const signer = provider.getSigner();
+  const optionmaker = new ethers.Contract(contractAddress, contractABI, signer);
+
+  try {
+    const tx = await optionmaker.JDM_CALL_START_REPLICATION(JDM_Call_Input);
+    // wait until the transaction is mined
+    await tx.wait();
+
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
+    };
+  }
 };
 
 // export const mintNFT = async (url, name, description) => {
@@ -60,7 +139,6 @@ export const connectWallet = async () => {
 // 	metadata.name = name;
 // 	metadata.image = url;
 // 	metadata.description = description;
-
 
 // 	//make pinata call
 // 	const pinataResponse = await pinJSONToIPFS(metadata);
@@ -101,48 +179,44 @@ export const connectWallet = async () => {
 
 // }
 
-
-
 export const getCurrentWalletConnected = async () => {
-	if (window.ethereum) {
-		try {
-			const addressArray = await window.ethereum.request({
-				method: "eth_accounts",
-			});
-			if (addressArray.length > 0) {
-				return {
-					address: addressArray[0],
-          			status: "👆🏽 Write a message in the text-field above.",
-				};
-			} else {
-				return {
-					address: "",
-		            status: "🦊 Connect to Metamask using the top right button.",
-				};
-			}
-		} catch (err) {
-			return {
-				address: "",
-		        status: "😥 " + err.message,
-			};
-		}
-	} else {
-		return {
-			address: "",
-			status: (
-				<span>
-					<p>
-						{" "}
-			          	  🦊{" "}
-			          	  <a target="_blank" href={`https://metamask.io/download.html`}>
-			          	  You must install Metamask, a virtual Ethereum wallet, in your
-			          	  browser.
-			          	  </a>
-		          	  </p>
-	          	  </span>
-				),
-		};
-	}
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "👆🏽 Write a message in the text-field above.",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
 };
-
-
