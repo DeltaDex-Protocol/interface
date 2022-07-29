b94382622b0c8d96dc7cf509c707a9892bc20a1b
Кирилл Богомолов <kirill.bogomolov95@gmail.com>
2022-07-29 12:14:34 +0300

    all positions

--- src/Components/Positions/AllPositions.jsx
+++ src/Components/Positions/AllPositions.jsx
@@ -1,126 +1,247 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+// import other component
+import Titles from "./../Titles/Titles";
+import FormInput from "./../Forms/FormInput/FormInput";
+
+// import other pkg
+import { Form, Button } from "react-bootstrap";
+// import { useFormik } from "formik";
+import { string, object, ref } from "yup";
+
+import {
+  getUserPositions,
+  getUserPositionsTable,
+  getCurrentPositions,
+} from "./../../utils/interact";
+
+import Creatable, { useCreatable } from "react-select/creatable";
+import DataGrid from "react-data-grid";
+import { useEffect } from "react";
+
+const TokenOptions = [
+  { label: "Black Scholes", value: "BSM" },
+  { label: "Jump Diffusion Model", value: "JDM" },
+  { label: "Heston Model", value: "HM" },
+  { label: "SABR Model", value: "SABR" },
+];
+
+const columns = [
+  { key: "id", name: "ID" },
+  { key: "token0", name: "Token 1" },
+  { key: "token1", name: "Token 2" },
+  { key: "token0_balance", name: "Token 1 Balance" },
+  { key: "token1_balance", name: "Token 2 Balance" },
+  { key: "amount", name: "Initial Deposit" },
+  { key: "expiry", name: "Time to expiry" },
+  { key: "fees", name: "Fees" },
+  { key: "lastHedge", name: "Last Hedge" },
+  { key: "strike", name: "K" },
+  { key: "T", name: "T" },
+  { key: "r", name: "r" },
+  { key: "sigma", name: "sigma" },
+  { key: "lam", name: "lam" },
+  { key: "m", name: "m" },
+  { key: "v", name: "v" },
+];
 
 
-// for (i < length): <row component>, in the row component should be another component with metadata. And if state has changed, it should be rendered.
+
+const defineAddresses = {
+  '0x6B175474E89094C44Da98b954EedeAC495271d0F': "DAI",
+  '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': "USDC",
+  '0xdAC17F958D2ee523a2206206994597C13D831ec7': "USDT",
+  '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': "WETH",
+}
 
 
-const AllPositions = () => {
+const GenerateRow = ({row}) => {
 
-    const [rowsExpanded, setExpandedRows] = useState({});
+  const [isVisible, setVisibility] = useState(false);
+  // console.log(defineAddresses.)
 
+  return (
+    <>
+    <tr className="bg-gray-00  hover:bg-gray-300 text-center" id={row.id} onClick={() => setVisibility(!isVisible)}>
+      <th scope="row" className="py-4 px-6  whitespace-nowrap justify-center">
+          {row.id} 
+      </th>
+      <td className="py-4 px-6">
+          {defineAddresses[row.token0] + "-" + defineAddresses[row.token1]}
+      </td>
+      <th scope="row" className="py-4 px-6  whitespace-nowrap justify-center">
+          {parseFloat(row.token0_balance).toFixed(3)}
+      </th>
+      <th scope="row" className="py-4 px-6  whitespace-nowrap justify-center">
+          {parseFloat(row.token1_balance).toFixed(3)}
+      </th>
 
-    return (
-        
-<div className="overflow-x-auto relative sm:rounded-lg shadow-lg">
-    <table className="w-full text-sm text-left text-blue-400  shadow-lg">
-        <thead className="text-xs text-black uppercase bg-gray-400">
-            <tr>
-                <th scope="col" className="py-3 px-6">
-                    Position id
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Option pair
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Option type
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Direction
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Strike
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Expiry
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Last hedge
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Details
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            
-            <tr className="bg-gray-00  hover:bg-gray-300 text-black">
-                <th scope="row" className="py-4 px-6 font-medium  whitespace-nowrap ">
-                    Apple 
-                </th>
-                <td className="py-4 px-6">
-                    Sliver
-                </td>
-                <td className="py-4 px-6">
-                    Laptop
-                </td>
-                <td className="py-4 px-6">
-                    $2999
-                </td>
-                <td className="py-4 px-6">
-                    <button 
-                    className="font-medium text-black rounded-lg bg-gray-300"
-                    onClick={() => setExpandedRows({1: !rowsExpanded[1]})}
-                    >Show</button>
-                </td>
-            </tr>
+      <td className="py-4 px-6">
+          {row.isCall ? "call" : "put"}
+      </td>
+      <td className="py-4 px-6">
+          {row.isLong ? "long" : "short"}
+      </td>
+      <td>
+        {row.strike}
+      </td>
+      <td>
+        {new Date(parseInt(row.expiry) * 1000).toISOString().slice(0, 10).replace('T', ' ')}
+      </td>
+      <td>
+        {new Date(parseInt(row.lastHedge) * 1000).toISOString().slice(0,10)}
+      </td>
+      <td className="py-4 px-3">
+          <button 
+          className="font-medium text-black rounded-lg bg-gray-300"
+          onClick={() => setVisibility(!isVisible)}
+          >Show</button>
+      </td>
+    </tr>
+    {(isVisible !== false) && (
+        <>
+              <tr className=" bg-gray-300 text-center">
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">fees</span>
+                      <br/>
+                      {row.fees}
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">amount</span>
+                      <br/>
+                      {parseFloat(row.amount).toFixed(3)}
+                  </th>
+                  <th scope="col" colspan="1" className="py-3 px-6 ">
+                      <span className="text-black">hedge fee</span>
+                      <br/>
+                      {parseFloat(row.hedgeFee).toFixed(3)} 
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">hedges per day</span>
+                      <br/>
+                      {row.perday}
+                  </th>
+                  <th/>
+                  <th/>
+                  <th/>
+                  <th/>
+                  <th/>
+                  <th/>
+              </tr>
+              <tr className=" bg-gray-300 text-center">
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">model</span>
+                      <br/>
+                      {"jump diffusion"}
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">lambda</span>
+                      <br/>
+                      {parseFloat(row.lam).toFixed(3)}
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">m</span>
+                      <br/>
+                      {parseFloat(row.m).toFixed(3)}
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">risk-free rate</span>
+                      <br/>
+                      {parseFloat(row.r).toFixed(3)}
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">sigma</span>
+                      <br/>
+                      {parseFloat(row.sigma).toFixed(3)}
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      <span className="text-black">nu</span>
+                      <br/>
+                      {parseFloat(row.v).toFixed(3)}
+                  </th>
+                  <th/>
+                  <th/>
+                  <th/>
+                  <th/>
+              </tr>
+          </>
 
-            {(rowsExpanded[1] !== undefined) && (rowsExpanded[1] !== false) && (
-
-            <tr className="text-black bg-gray-300">
-                <th scope="col" className="py-3 px-6">
-                    Product name<br/>
-                    asdas
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Color
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Category
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Price
-                </th>
-                <th/>
-            </tr>
-
-            // <tr className="bg-gray-00  hover:bg-gray-300 text-black">
-            //     <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap">
-            //         Microsoft Surface Pro
-            //     </th>
-            //     <td className="py-4 px-6">
-            //         White
-            //     </td>
-            //     <td className="py-4 px-6">
-            //         Laptop PC
-            //     </td>
-            //     <td className="py-4 px-6">
-            //         $1999
-            //     </td>
-            // </tr>
-            )}
-            
-            <tr className="bg-gray-00  hover:bg-gray-300 text-black">
-                <th scope="row" className="py-4 px-6 font-medium whitespace-nowrap ">
-                    Apple Watch 5
-                </th>
-                <td className="py-4 px-6">
-                    Red
-                </td>
-                <td className="py-4 px-6">
-                    Wearables
-                </td>
-                <td className="py-4 px-6">
-                    $999
-                </td>
-                <td className="py-4 px-6">
-                    <a href="#" className="font-medium text-black hover:underline">Edit</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-
+    )}
+    </>
     )
 }
 
+
+// const rows = ({}) => getRows();
+
+const rows = [{}];
+
+// const rows = getUserPositionsTable();
+
+const AllPositions = ({}) => {
+  const [ReplicationModel, setReplicationModel] = useState("");
+
+  const [tagInputValue, setTagInputValue] = useState("");
+  const [tagValue, setTagValue] = useState("");
+
+  const [rowData, setRowData] = useState([]);
+
+  const [upd, setUpd] = useState(false);
+
+  const [rowsExpanded, setExpandedRows] = useState({});
+
+
+  useEffect(() => {
+    const func = async () => {
+      const rowData = await getUserPositions();
+      console.log(rowData);
+      setRowData(rowData);
+    };
+    func();
+  }, [upd]);
+
+  return (
+    <div className="overflow-x-auto relative sm:rounded-lg shadow-lg mb-8">
+      <table className="w-full text-sm text-left  shadow-lg">
+          <thead className="text-xs text-black uppercase bg-gray-400 text-center">
+              <tr>
+                  <th scope="col" className="py-3 px-6">
+                      Position id
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      Option pair
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      Reward for hedging
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      Estimated tx cost for hedging
+                  </th>
+                  <th/>
+                  <th/>
+                  <th/>
+                  <th scope="col" className="py-3 px-6">
+                      Expiry
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                      Last hedge
+                  </th>
+
+                  <th scope="col" className="py-3 px-6">
+                      Details
+                  </th>
+              </tr>
+          </thead>
+          {rowData[0] !== undefined && (
+          <tbody>
+              {rowData.map((el, i) => <GenerateRow row={rowData[i]}/>)}
+          </tbody>
+          )}
+      </table>
+    </div>
+  );
+};
+
 export default AllPositions;
+
+
