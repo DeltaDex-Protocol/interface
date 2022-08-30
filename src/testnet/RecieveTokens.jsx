b5a2f63a5b3f588e097d8c26a772251c8a9024f3
partylikeits1983 <aleqvids@gmail.com>
2022-08-30 19:23:39 +0300

    text

--- src/testnet/RecieveTokens.jsx
+++ src/testnet/RecieveTokens.jsx
@@ -3,101 +3,106 @@ import { useEffect, useState } from "react";
 import { connectWallet, getCurrentWalletConnected } from "./connectWallet";
 
 const RecieveTokens = () => {
-    const [walletAddress, setWallet] = useState("");
-    const [status, setStatus] = useState("");
-  
-    useEffect(async () => {
-      const { address, status } = await getCurrentWalletConnected();
-  
-      setWallet(address);
-      setStatus(status);
-  
-      addWalletListener();
-    }, []);
-  
-    function addWalletListener() {
-      if (window.ethereum) {
-        window.ethereum.on("accountsChanged", (accounts) => {
-          if (accounts.length > 0) {
-            setWallet(accounts[0]);
-            setStatus("👆🏽 Write a message in the text-field above.");
-          } else {
-            setWallet("");
-            setStatus("🦊 Connect to Metamask using the top right button.");
-          }
-        });
-      } else {
-        setStatus(
-          <p>
-            {" "}
-            🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
-              You must install Metamask, a virtual Ethereum wallet, in your
-              browser.
-            </a>
-          </p>
-        );
-      }
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(async () => {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
     }
-  
-    const connectWalletPressed = async () => {
-      const walletResponse = await connectWallet();
-      setStatus(walletResponse.status);
-      setWallet(walletResponse.address);
-    };
+  }
 
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
 
-    return (
-        <div className="bg-gradient-to-r from-slate-900 to-indigo-900 h-screen ">
-            {/* <div className="max-w-6xl">
+  return (
+    <div className="bg-gradient-to-r from-slate-900 to-indigo-900 h-screen ">
+      {/* <div className="max-w-6xl">
                 <span>
                 DeltaDex
                 </span>
             </div> */}
-            <div className="max-w-6xl max-h-screen mx-auto flex justify-between">
-                <div class="flex-col flex  max-w-md ml-10 lg:ml-0 z-10">
-                    <div class=" flex flex-col  text-white my-52">
-                        <h1 class=" font-semibold text-4xl ">Receive testnet <br/> tokens for free</h1>
-                        <p class="pr-3 text-sm max-w-sm">Lorem ipsum is placeholder text commonly used in the graphic, print,
-                            and publishing industries for previewing layouts and visual mockups</p>
-                    </div>
-                </div>
+      <div className="max-w-6xl max-h-screen mx-auto flex justify-between">
+        <div class="flex-col flex  max-w-md ml-10 lg:ml-0 z-10">
+          <div class=" flex flex-col  text-white my-52">
+            <h1 class=" font-semibold text-4xl ">
+              Receive testnet <br /> tokens
+            </h1>
+            <p class="pr-3 text-sm max-w-sm">
+              Connect you metamask wallet, mint DAI and WETH, approve the
+              DeltaDex contract.
+            </p>
+          </div>
+        </div>
 
-                <div className="mx-auto mt-40">
-                    <span className="text-white text-xl">Get free DeltaDex DAI and WETH tokens</span>
-                    <div className="flex space-x-32 mr-10 lg:ml-0 mt-10">
-                        <span className="my-auto text-white text-3xl w-max">Step 1.</span>
-                        <div className="rounded-xl py-2 px-3 text-lg text-white text-center font-semibold bg-violet-500"
-                            onClick={connectWallet}>
-                            {walletAddress !== "Not connected" ? (
-                                <span>Not connected</span>
-                            ) : (
-                                <span>Connected</span>
-                            )}
-                        </div>
-                    </div>
-                    <div className="flex space-x-32 mr-10 lg:ml-0 mt-10">
-                        <span className="my-auto text-white text-3xl w-max">Step 2.</span>
-                        <div className="rounded-xl py-2 px-3 text-lg text-black text-center font-normal bg-white">
-                        {walletAddress !== "Not connected" ? (
-                            String(walletAddress).substring(0, 6) +
-                            "..." +
-                            String(walletAddress).substring(38)
-                        ) : (
-                            <span>Connect Wallet</span>
-                        )}
-                        </div>
-                    </div>
-                    <span>
-
-                    </span>
-                    <div className="flex space-x-32 mr-10 lg:ml-0 mt-10">
-                        <span className="my-auto text-white text-3xl w-max">Step 3.</span>
-                        <div className="rounded-xl py-2 px-3 text-lg text-white text-center font-semibold bg-violet-500">
-                            <span className="">Check your address</span>
-                        </div>
-                    </div>
-                </div>
+        <div className="mx-auto mt-40">
+          <span className="text-white text-xl">
+            Get free DeltaDex DAI and WETH tokens
+          </span>
+          <div className="flex space-x-32 mr-10 lg:ml-0 mt-10">
+            <span className="my-auto text-white text-3xl w-max">Step 1.</span>
+            <div
+              className="rounded-xl py-2 px-3 text-lg text-white text-center font-semibold bg-violet-500"
+              onClick={connectWallet}
+            >
+              {walletAddress !== "Not connected" ? (
+                <span>Not connected</span>
+              ) : (
+                <span>Connected</span>
+              )}
+            </div>
+          </div>
+          <div className="flex space-x-32 mr-10 lg:ml-0 mt-10">
+            <span className="my-auto text-white text-3xl w-max">Step 2.</span>
+            <div className="rounded-xl py-2 px-3 text-lg text-black text-center font-normal bg-white">
+              {walletAddress !== "Not connected" ? (
+                String(walletAddress).substring(0, 6) +
+                "..." +
+                String(walletAddress).substring(38)
+              ) : (
+                <span>Connect Wallet</span>
+              )}
+            </div>
+          </div>
+          <span></span>
+          <div className="flex space-x-32 mr-10 lg:ml-0 mt-10">
+            <span className="my-auto text-white text-3xl w-max">Step 3.</span>
+            <div className="rounded-xl py-2 px-3 text-lg text-white text-center font-semibold bg-violet-500">
+              <span className="">Check your address</span>
+            </div>
+          </div>
+        </div>
 
         {/* <div class="w-full max-w-xs">
           <form class="bg-slate shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
