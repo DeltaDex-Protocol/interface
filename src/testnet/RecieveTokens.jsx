152684f46ada8e179bab7e08c5eda6643fa7e40d
partylikeits1983 <aleqvids@gmail.com>
2022-08-30 19:10:34 +0300

    metamask wallet

--- src/testnet/RecieveTokens.jsx
+++ src/testnet/RecieveTokens.jsx
@@ -1,23 +1,83 @@
 import { useEffect, useState } from "react";
 
-const RecieveTokens = () => {
-    return (
-        <div className="bg-gradient-to-r from-slate-900 to-indigo-900 h-screen">
-            <div className="max-w-6xl max-h-screen mx-auto flex justify-between">
-                <div class="flex-col flex  self-center lg:p-14 sm:max-w-4xl xl:max-w-md sm:ml-10 lg:ml-0 z-10">
-                    <div class="self-start hidden sm:flex flex-col  text-white">
-                        <h1 class="my-3 font-semibold text-4xl">Receive free <br/> DeltaDex tokens</h1>
-                        <p class="pr-3 text-sm ">Lorem ipsum is placeholder text commonly used in the graphic, print,
-                            and publishing industries for previewing layouts and visual mockups</p>
-                    </div>
-                </div>
+import { connectWallet, getCurrentWalletConnected } from "./connectWallet";
 
-                <div className="mx-auto my-auto flex flex-col">
-                    <div className="flex space-x-24">
-                        <span className="my-auto text-white text-3xl">Step 1.</span>
-                        <button className="rounded-xl text-lg  text-white font-semibold bg-violet-500">Connect your wallet</button>
-                    </div>
-                </div>
+const RecieveTokens = () => {
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(async () => {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  return (
+    <div className="bg-gradient-to-r from-slate-900 to-indigo-900 h-screen">
+      <div className="max-w-6xl max-h-screen mx-auto flex justify-between">
+        <div class="flex-col flex  self-center lg:p-14 sm:max-w-4xl xl:max-w-md sm:ml-10 lg:ml-0 z-10">
+          <div class="self-start hidden sm:flex flex-col  text-white">
+            <h1 class="my-3 font-semibold text-4xl">
+              Receive free <br /> DeltaDex tokens
+            </h1>
+            <p class="pr-3 text-sm ">
+              Lorem ipsum is placeholder text commonly used in the graphic,
+              print, and publishing industries for previewing layouts and visual
+              mockups
+            </p>
+          </div>
+        </div>
+
+        <div className="mx-auto my-auto flex flex-col">
+          <div className="flex space-x-24">
+            <span className="my-auto text-white text-3xl">Step 1.</span>
+
+            <button onClick={connectWallet}>
+              {walletAddress !== "Not connected" ? (
+                "Connected: " +
+                String(walletAddress).substring(0, 6) +
+                "..." +
+                String(walletAddress).substring(38)
+              ) : (
+                <span>Connect Wallet</span>
+              )}
+            </button>
+          </div>
+        </div>
 
         {/* <div class="w-full max-w-xs">
           <form class="bg-slate shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
--- src/testnet/connectWallet.js
+++ src/testnet/connectWallet.js
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+
 const erc20ABI = require("./ERC20.json");
 const erc20Address = "0x2dc042385a6b1efaeec4816118e704028a733bed";
 
 var ethers = require("ethers");
 const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+/* export const connectWalletPressed = async () => {
+  const walletResponse = await connectWallet();
+  setStatus(walletResponse.status);
+  setWallet(walletResponse.address);
+}; */
 
 export const connectWallet = async () => {
   if (window.ethereum) {
@@ -15,6 +23,48 @@ export const connectWallet = async () => {
         address: addressArray[0],
       };
       return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "🦊 Successfully connected",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button.",
+        };
+      }
     } catch (err) {
       return {
         address: "",
@@ -66,7 +116,6 @@ export const MintTokens = async (erc20Address, amount) => {
     };
   }
 };
-
 
 export const ApproveTokens = async (erc20Address, amount) => {
   amount = ethers.utils.parseUnits(amount);
