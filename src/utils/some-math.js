b6ef71109880ffe3094231bb8e56f925ac181fb8
kirrya95 <kirill.bogomolov95@gmail.com>
2022-09-11 17:40:19 +0300

    initial next

--- src/utils/some-math.js
+++
@@ -1,8 +0,0 @@
-const mathjs = require('mathjs');
-
-
-function cdfNormal (x, mean=0, standardDeviation=1) {
-  return (1 - mathjs.erf((mean - x ) / (Math.sqrt(2) * standardDeviation))) / 2
-}
-
-export default cdfNormal;
