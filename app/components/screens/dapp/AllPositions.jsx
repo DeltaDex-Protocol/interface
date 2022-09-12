f7c36613b2db8a00fab98075217d2bd8c5806f65
kirrya95 <kirill.bogomolov95@gmail.com>
2022-09-13 02:31:22 +0300

    all positions

--- app/components/screens/dapp/AllPositions.jsx
+++ app/components/screens/dapp/AllPositions.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState } from "react";
+import Banner from '../../dapp/Banner'
+import Table from '../../dapp/Tables/AllPositions/Table';
 
-export function AllPositions() {
+
+
+export function AllPositions({ ethPrice }) {
   return (
-    <div>AllPositions</div>
+    <div className=" ">
+      <div className="flex justify-between ">
+        <div className="mx-auto md:mx-0">
+          <Banner Unit={'AllPositions'} />
+        </div>
+        {/* <div className='invisible lg:visible'>
+            <PriceChart data={ethPrice}/>
+        </div> */}
+      </div>
+      <div className="mx-auto md:mx-0 overflow-x-scroll max-w-xs sm:max-w-md md:max-w-xl lg:max-w-4xl xl:max-w-5xl ">
+        <Table />
+      </div>
+    </div>
   )
 }
 
--- app/components/screens/dapp/MyPositions.jsx
+++ app/components/screens/dapp/MyPositions.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import { useEffect } from "react"
+import { useEffect } from 'react'
 import ReactLoading from 'react-loading'
 import { defineAddresses } from '../../../configs/mypositions.config'
-import Table from '../../dapp/TablePositions/Table'
+import Table from '../../dapp/Tables/MyPositions/Table'
 import Banner from '../../dapp/Banner'
 import PriceChart from '../../../shared/PriceChart'
 
 
 
-
 export function MyPositions({ ethPrice }) {
-
   return (
-    <div className=' '>
+    <div className=" ">
       <div className="flex justify-between ">
-        <div className='mx-auto md:mx-0'>
-          <Banner Unit={'YourPositions'}/>
+        <div className="mx-auto md:mx-0">
+          <Banner Unit={'YourPositions'} />
         </div>
         {/* <div className='invisible lg:visible'>
             <PriceChart data={ethPrice}/>
         </div> */}
       </div>
-      <div className='mx-auto md:mx-0 overflow-x-scroll max-w-xs sm:max-w-md md:max-w-xl lg:max-w-4xl xl:max-w-5xl '>
+      <div className="mx-auto md:mx-0 overflow-x-scroll max-w-xs sm:max-w-md md:max-w-xl lg:max-w-4xl xl:max-w-5xl ">
         <Table />
       </div>
     </div>
