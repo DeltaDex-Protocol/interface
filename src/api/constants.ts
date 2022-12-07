ffc979ade6d1577d50a25bca895a4261d9c5f5a6
szabo <aleqvids@gmail.com>
2022-12-07 14:01:31 +0300

    update link

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -5,5 +5,5 @@ export const DAI = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063'
 export const WETH = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
 export const daiWHALE = '0x1a34edac6ab1cf8fe609f163d798c8644636c7dd' // not sure if it's really needed
 export const wethWHALE = '0xCe2CC46682E9C6D5f174aF598fb4931a9c0bE68e' // not sure if it's really needed
-export const OptionStorageAddress = '0xd7a89AEa304A491Ef4B5e74928370059fa53D8C6'
-export const CoreAddress = '0x17a638B1ba87B30b4768cf0252Dffc271368E324'
+export const OptionStorageAddress = '0xcFAEccc723068bFe3D86D571494fB1602E1de142'
+export const CoreAddress = '0xb628BA2f064211492eB9f799a83123b156Fba3Ba'
